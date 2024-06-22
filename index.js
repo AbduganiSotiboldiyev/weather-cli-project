e771cad278596a6e8e8cd007423345b98c7639c6
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-23 01:36:59 +0900

    working with  process.argv

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const getArgs = require("./helpers/arg")
 const startCli = () => {
     const args = getArgs(process.argv)
     console.log(args)
+    if(args.s){
+        // save
+    }
+    if(args.h){
+        // help
+    }
+    if(args.t){
+        // save token
+    }
+
+    // return result
 }
 
 startCli()
